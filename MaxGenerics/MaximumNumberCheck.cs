21d49cefd795e58da4549d9e918a39430a23a36f
ovi2410 <guneshwarifegade@gmail.com>
2022-07-01 14:56:23 +0530

    [ovi]Added:UC add refactor1

---
+++ MaxGenerics/MaximumNumberCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxGenerics
+{
+    internal class MaximumNumberCheck<T> where T : IComparable
+    {
+        public static T MaximumStringNumber(T firstString, T secondString, T thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+
+
+    }
+}
+    
+
---
+++ MaxGenerics/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using MaxGenerics;
+
+Console.WriteLine("---- Find max ---- ");
+
+int output1 = MaximumNumberCheck<int>.MaximumStringNumber(15, 32, 64);
+Console.WriteLine("Maximum int [TC.1]:- " + output1);
+
+float output2 = MaximumNumberCheck<float>.MaximumStringNumber(45.5f, 98.54f, 56.21f);
+Console.WriteLine("Maximum float [TC.1]:- " + output2);
+
+string output3 = MaximumNumberCheck<string>.MaximumStringNumber("Peach", "Banana", "Apple");
+Console.WriteLine("Maximum String [TC.1]:- " + output3);
+
